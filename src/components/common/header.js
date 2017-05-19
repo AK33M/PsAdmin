5a47d5efed85b3ce21442107eed3f1784532a3c3
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-05-19 15:59:36 +0100

    ...nav

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -6,17 +6,23 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 var Header = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <Link to="app" className="navbar-brand">
                         <img src="images/pluralsight-logo.png" alt="Pluralsight logo"/>
                     </Link>
-                   <ul className="nav navbar-nav">
-                        <li><Link to="app">Home</Link></li>
-                        <li><Link to="authors">Authors</Link></li>
-                        <li><Link to="about">About</Link></li>                        
+                    <ul className="nav nav-pills">
+                        <li>
+                            <Link to="app">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="authors">Authors</Link>
+                        </li>
+                        <li>
+                            <Link to="about">About</Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
