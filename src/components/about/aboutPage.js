cf2c940ff5cf5dc707db07c2a063413172a301a1
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-05-19 15:10:43 +0100

    React Router

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -2,8 +2,22 @@
 
 var React = require('react');
 
-var About = React.createClass({
-    render: function (){
+var AboutPage = React.createClass({
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+                transition.about();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function (transition, component) {
+            if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+                transition.about();
+            }
+        }
+    },
+    render: function () {
         return (
             <div>
                 <h1>About</h1>
@@ -24,4 +38,4 @@ var About = React.createClass({
     }
 });
 
-module.exports = About;
+module.exports = AboutPage;
